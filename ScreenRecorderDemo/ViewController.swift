3719caed743ad79b973a8010e19760f14101b9e3
tuxi <seyooe@gmail.com>
2020-12-01 10:57:37 +0800

    Initial Commit

---
+++ ScreenRecorderDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScreenRecorderDemo
+//
+//  Created by xiaoyuan on 2020/12/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
